c482c9abcf42e634ebcdb196b1525511c35bb113
LevonPython <levon.python@gmail.com>
2021-05-21 17:31:39 +0400

    Added recording feature

--- rec_developed.py
+++ rec_developed.py
@@ -1,5 +1,6 @@
 import pyaudio
 import wave
+from datetime import datetime
 # pip install pipwin
 # pipwin install pyaudio
 
@@ -8,29 +9,40 @@ pyaudio gives you more low-level control, it is possible to get and set paramete
 and to check your CPU load and input or output latency.
 """
 
+rec_status = int(input("Do you want to record? Insert 1 or 0: "))
+print(f"rec_status: {rec_status}")
+
 chunk = 1024  # Record in chunks of 1024 samples
 sample_format = pyaudio.paInt16  # 16 bits per sample
 channels = 2
 fs = 44100  # Record at 44100 samples per second
-seconds = 5
-filename = "records/output.wav"
+seconds = 1
+file_date = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+filename = f"records/record_{file_date}.wav"
+print(filename)
 
 p = pyaudio.PyAudio()  # Create an interface to PortAudio
-
-print('Recording')
 
 stream = p.open(format=sample_format,
                 channels=channels,
                 rate=fs,
                 frames_per_buffer=chunk,
                 input=True)
-
 frames = []  # Initialize array to store frames
+seconds_count = 0
+while rec_status == 1:
 
-# Store data in chunks for 3 seconds
-for i in range(0, int(fs / chunk * seconds)):
-    data = stream.read(chunk)
-    frames.append(data)
+    print('Recording')
+    # data = stream.read(chunk)
+    # frames.append(data)
+    rec_status = int(input("Continue recording? Insert 1 or 0: "))
+    # Store data in chunks for specified time (seconds)
+    for i in range(0, int(fs / chunk * seconds)):
+        data = stream.read(chunk)
+        frames.append(data)
+    seconds_count += 1
+
+print(f"frames {frames}")
 
 # Stop and close the stream
 stream.stop_stream()
@@ -47,3 +59,4 @@ wf.setsampwidth(p.get_sample_size(sample_format))
 wf.setframerate(fs)
 wf.writeframes(b''.join(frames))
 wf.close()
+print(f"{seconds_count} seconds recorded")
---
+++ test.py
@@ -0,0 +1,107 @@
+import argparse, sys
+import tkinter as tk
+import tkinter.messagebox as tkMessageBox
+
+def update_timeText():
+    global timer, window_title
+    if state:
+        if countdown:
+            if (timer[0] == 0 and timer[1] <= 1):
+                reset()
+                tkMessageBox.showinfo(title=window_title,
+                        message='Time out!')
+                return #Timer exited, return and do nothing.
+            # Decrement a second at every call to this function
+            timer[1] -= 1
+            if (timer[1] <= 0):
+                timer[0] -= 1
+                timer[1] = 59
+        else:
+            # Every time this function is called, increment 1 second
+            timer[1] += 1
+            if (timer[1] >= 60):
+                timer[0] += 1
+                timer[1] = 0
+
+        # ----------- Common for both countdown and countup
+        timeString = pattern.format(timer[0], timer[1])
+
+        # Update the timeText Label box with the current time
+        timeText.configure(text=timeString)
+
+    # Call the update_timeText() function after 1 second
+    root.after(1000, update_timeText)
+
+def start():
+    global state
+    state = True
+
+def pause():
+    global state
+    state = False
+
+def reset():
+    global state, timer, countdown, pattern
+    state = False
+    if countdown:
+        timer = [timelimit , 0]
+        timestr = pattern.format(timer[0], timer[1])
+    else:
+        timer = [0, 0]
+        timestr = '00:00'
+
+    timeText.configure(text=timestr)
+
+def quitclock():
+    root.destroy()
+
+def gt_zero(s):
+    try:
+        if int(s) > 0:
+            return int(s)
+        else:
+            raise argparse.ArgumentTypeError('Time limit must be an int > 0')
+    except:
+        raise argparse.ArgumentTypeError('Time limit must be an int > 0')
+
+if __name__=='__main__':
+    countdown = False
+    timestr = ''
+    parser = argparse.ArgumentParser(description='Simple timer')
+    parser.add_argument('--name', '-n', help='Name of the timer widget',
+            default=' <=-=- Simple timer -=-=> ')
+    parser.add_argument('--reverse', '-r', type = gt_zero,
+            help='Countdown timer with time limit (>0)')
+    parser.add_argument('--size', '-s',
+            help='Font size for the timer', default='75')
+    values = parser.parse_args()
+    window_title = values.name
+    font_size = values.size
+
+    if values.reverse:
+        countdown = True
+        timelimit = int(values.reverse)
+
+    # Simple status flag; False => timer is not running, True otherwise
+    state = False
+    # For the padding format
+    pattern = '{0:02d}:{1:02d}'
+
+    root = tk.Tk()
+    root.wm_title(window_title)
+
+    # Create a timeText Label (a text box)
+    timeText = tk.Label( root,
+            text = timestr, font = ("Sans",font_size),
+            background = 'black', foreground = 'green')
+    timeText.pack(fill='x')
+
+    buttons = [ ('Start/Unpause', start), ('Pause', pause),
+            ('Reset', reset), ('Quit', quitclock) ]
+
+    for btn in buttons:
+        tk.Button(root, text = btn[0], command=btn[1]).pack(fill='x')
+
+    reset()
+    update_timeText()
+    root.mainloop()
